88a20441d5336973818cbdec1b80ddf429d2a941
Shivam Gupta <shivam.gupta1@metacube.com>
2024-01-16 12:23:42 +0530

    Cart component Completed

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,20 @@
 import './cart-dropdown.style.scss';
 import Button from '../button/button.component';
-
+import CartItem from '../cart-item/cart-item.component'
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.contexts';
 
 const CartDropDown =() => {
     
+    const { cartItems }= useContext(CartContext);
     return  ( 
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>Go to CheckOut</Button>
+                {cartItems.map ((item) =>(
+                    <CartItem key={item.id} cartItem = {item} />
+                ))}
             </div>
+            <Button>Go to CheckOut</Button>
 
         </div>
     )
